23599a74857b6dab23eae4fdf35c685d904a880e
hammadmirzaa <hamadbaig25@gmail.com>
2024-06-27 20:24:21 +0500

    updated swatch

--- matrix/src/Layout/content.jsx
+++ matrix/src/Layout/content.jsx
@@ -14,16 +14,16 @@ function Content({ activeData }) {
             ease: 'power3.inOut',
             duration: 0.8,
           });
-          gsap.from('.text', {
-            y: 200,
-            ease: 'power4.out',
-            duration: 1,
-            stagger:{
-                amount:0.3,
-            },
-        });
+        //   gsap.from('.text', {
+        //     y: 200,
+        //     ease: 'power4.out',
+        //     duration: 1,
+        //     stagger:{
+        //         amount:0.3,
+        //     },
+        // });
         return () => {};
-    }, []);
+    }, [activeData]);
    return (
     <div className="select-none w-full h-2/5 flex justify-center items-center lg:w-1/2 lg:h-full lg:justify-end ">
       <div className=" flex justify-start flex-col items-start w-2/3 ">
--- matrix/src/Layout/index.jsx
+++ matrix/src/Layout/index.jsx
@@ -1,18 +1,43 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import gsap from 'gsap';
 import Content from './content';
 import { data } from '../data';
 import Canvas from './canvas';
 function Banner() {
   const banner = useRef();
-  const [activeData, setActiveData] = useState(data[0]);
+  const [activeData, setActiveData] = useState(data[1]);
+
+  const handleSwatch = (item)=>{
+    if(activeData.id!== item.id){
+      setActiveData(item);
+    }
+  }
+
+  useEffect(() => {
+    gsap.to(banner.current,{
+      background:activeData.background, 
+      ease:'power3.inOut',
+      duration:0.8,
+    });
+    gsap.to('.logo',{
+      active:activeData.headingColor, 
+      ease:'power3.inOut',
+      duration:0.8,
+    });
+  
+    return () => {
+
+    }
+  }, [activeData])
+  
+
   return (
     <div ref={banner} className='w-screen h-screen relative' >
     <div className="logo absolute my-2 ml-6 text-left text-2xl font-bold tracking-widest md:ml-28 lg:ml-[12vw] lg:my-8">
     MISFIT.
   </div>
   <div className="w-full h-full flex justify-between items-center flex-col lg:flex-row-reverse">
-  <Canvas activeData={activeData} swatchData={data} />
+  <Canvas activeData={activeData} swatchData={data} handleSwatch={handleSwatch} />
   <Content activeData={activeData} /> 
   </div>
     </div>
