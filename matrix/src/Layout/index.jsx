15813f4fd74ab850641f877013343971fc7ef5f7
hammadmirzaa <hamadbaig25@gmail.com>
2024-06-27 19:56:41 +0500

    completed Content Animation

--- matrix/src/Layout/index.jsx
+++ matrix/src/Layout/index.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React, { useRef, useState } from 'react';
+import gsap from 'gsap';
+import Content from './content';
+import { data } from '../data';
+import Canvas from './canvas';
 function Banner() {
+  const banner = useRef();
+  const [activeData, setActiveData] = useState(data[0]);
   return (
-    <div>Banner</div>
+    <div ref={banner} className='w-screen h-screen relative' >
+    <div className="logo absolute my-2 ml-6 text-left text-2xl font-bold tracking-widest md:ml-28 lg:ml-[12vw] lg:my-8">
+    MISFIT.
+  </div>
+  <div className="w-full h-full flex justify-between items-center flex-col lg:flex-row-reverse">
+  <Canvas activeData={activeData} swatchData={data} />
+  <Content activeData={activeData} /> 
+  </div>
+    </div>
   )
 }
 
-export default Banner
+export default Banner;
