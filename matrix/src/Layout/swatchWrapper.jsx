f1002085ced9b6e7b2d03891201fe5fdc4c9a355
hammadmirzaa <hamadbaig25@gmail.com>
2024-06-30 23:49:51 +0500

    fixed deployment issue

--- matrix/src/Layout/swatchWrapper.jsx
+++ matrix/src/Layout/swatchWrapper.jsx
@@ -11,7 +11,7 @@ function SwatchWrapper({ activeData, swatchData, handleSwatch }) {
 
   return (
     <div className="h-fit absolute z-20 w-full bottom-0 flex justify-center gap-8 mb-2  lg:w-fit lg:inset-y-[40%] lg:right-20 lg:flex-col">
-      {swatchData.map((o) => (
+      {swatchData.map((o) =>(
         <SingleSwatchWrapper
           key={o.id}
           item={o}
